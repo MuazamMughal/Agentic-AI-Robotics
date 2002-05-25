fa51eda148feb167ca7dda1d92ce73c84c96751b
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2002-05-25 05:28:49 +0500

    async hello agent

--- 02_hello_agents/sync_hello_agent/sync_op_ag/src/sync_op_ag/main.py
+++ 02_hello_agents/sync_hello_agent/sync_op_ag/src/sync_op_ag/main.py
@@ -1,3 +1,4 @@
+#first of all importing all the things
 import os
 from dotenv import load_dotenv
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
@@ -13,24 +14,29 @@ if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
 
 #Reference: https://ai.google.dev/gemini-api/docs/openai
+# step # 1 creating Provider for Gemini API
+# This is the client that will be used to call the Gemini API
 external_client = AsyncOpenAI(
     api_key=gemini_api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
+
+# step# 2 Create the model using the external client
+
 model = OpenAIChatCompletionsModel(
     model="gemini-2.0-flash",
     openai_client=external_client
 )
-
+#strp # 3 define the config at the run level
 config = RunConfig(
     model=model,
     model_provider=external_client,
     tracing_disabled=True
 )
-
+#step# 4 then creating agent
 agent: Agent = Agent(name="Assistant", instructions="You are a helpful assistant", model=model)
-
+# step# 5 then calling the agent using the runner
 result = Runner.run_sync(agent, "Hello, how are you.", run_config=config)
 
 print("\nCALLING AGENT\n")
