2e657e062664469268f18f19010ba9c4212de826
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-05-02 23:37:44 +0500

    async hello agent

--- 02_hello_agents/async_hello_agent/async_op_ag/src/async_op_ag/main.py
+++ 02_hello_agents/async_hello_agent/async_op_ag/src/async_op_ag/main.py
@@ -11,7 +11,7 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 # Check if the API key is present; if not, raise an error
 if not gemini_api_key:
-    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+    raise ValueError("GEMINI_API_KEY  is not set. Please ensure it is defined in your .env file.")
 
 #Reference: https://ai.google.dev/gemini-api/docs/openai
 external_client = AsyncOpenAI(
